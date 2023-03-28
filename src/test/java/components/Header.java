e56d5826c45939b48d9cc003856f5f8ded1076c9
Mr-BD <52127297+Mr-BD@users.noreply.github.com>
2023-03-28 21:45:04 +0300

    added Header elements

--- src/test/java/components/Header.java
+++ src/test/java/components/Header.java
@@ -1,12 +1,43 @@
 package components;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class Header {
     private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    @FindBy(id = "nav-link-home")
+    WebElement homeLink;
+
+    @FindBy(id = "nav-link-login")
+    WebElement loginLink;
 
     public Header(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(8));
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goToLoginPage() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginLink));
+        loginLink.click();
+
+
+    }
+
+    public void goToHomePage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
+        wait.until(ExpectedConditions.elementToBeClickable(homeLink));
+        homeLink.click();
     }
 }
