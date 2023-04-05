1c82c9402045984c762b84a725ce35b5388a3ab2
Mr-BD <52127297+Mr-BD@users.noreply.github.com>
2023-04-05 21:18:38 +0300

    Added CommentAPost test

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,24 +1,36 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class BaseTest {
 
+
+    public static final String RESOURCES_DIR = "src" + File.separator + "test" + File.separator + "java" + File.separator + "resources" + File.separator;
+    public static final String SCREENSHOTS_DIR = RESOURCES_DIR + "screenshots" + File.separator;
+    public static final String REPORTS_DIR = RESOURCES_DIR + "reports" + File.separator;
     public final String HOME_URL = "http://training.skillo-bg.com:4200";
     protected WebDriver driver;
 
 
     @BeforeSuite
-    public void setupSuite() {
+    public void setupSuite() throws IOException {
         WebDriverManager.firefoxdriver().setup();
+        cleanDirectory(SCREENSHOTS_DIR);
+        cleanDirectory(REPORTS_DIR);
     }
 
     @BeforeMethod
@@ -32,11 +44,35 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void cleanup() {
+    public void cleanup(ITestResult testResult) {
+        takeScreenshot(testResult);
         if (driver != null) {
             driver.quit();
         }
     }
 
+    public void takeScreenshot(ITestResult testResult) {
+        if (ITestResult.FAILURE == testResult.getStatus()) {
+            TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+            File screenshot = takesScreenshot.getScreenshotAs(OutputType.FILE);
+            String testName = testResult.getName();
+            try {
+                FileUtils.copyFile(screenshot, new File(SCREENSHOTS_DIR + testName + ".jpg"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void cleanDirectory(String directoryPath) throws IOException {
+        File directory = new File(directoryPath);
+        FileUtils.cleanDirectory(directory);
+        String[] fileList = directory.list();
+        if (fileList != null && fileList.length == 0) {
+            System.out.printf("All files are deleted in Directory: %s%n", directoryPath);
+        } else {
+            System.out.printf("Unable to delete the files in Directory:%s%n", directoryPath);
+        }
+    }
 
 }
---
+++ src/test/java/tests/CommentAPostTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import components.Header;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.PostsPage;
+
+public class CommentAPostTest extends BaseTest {
+
+    @Parameters({"username", "password"})
+    @Test
+    public void shouldBeAbleToLeaveACommentUnderAPost(String username, String password) {
+        //1. Login with existing user
+        Header header = new Header(driver);
+        header.goToLoginPage();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.verifyLoginUrl();
+        loginPage.login(username, password);
+        //2. Verify that we're on posts page (Homepage)
+        PostsPage postsPage = new PostsPage(driver);
+        postsPage.verifyPostsPageUrl();
+        //3. Click on the first post that appears
+        postsPage.clickOnTheFirstPost();
+        //4. Verify that the post is opened inside the modal
+        postsPage.waitForSelectedModalToBeVisible();
+        //5. Submit a new comment
+        String newComment = "ggg";
+        postsPage.fillCommentInput(newComment);
+        postsPage.submitForm();
+        //6. Verify that the last comment on the post matches the text of our comment
+        String newestComment = postsPage.selectNewestComment();
+        Assert.assertEquals(newestComment, newComment, "Latest comment did not match the one that you've created");
+    }
+
+}
